69c05c601aefc3c2b93b10f85b101c4b557529ef
Arian <126694213+ArianVangle@users.noreply.github.com>
2024-12-18 21:55:23 +0300

    Add files via upload

---
+++ sketch_temp.ino
@@ -0,0 +1,91 @@
+#include <DHT.h>
+#include <DHT_U.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define PIN_CE 9
+#define PIN_CSN 10
+#define CH_NUM 0x60
+RF24 radio(PIN_CE, PIN_CSN);
+byte address[][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"};
+DHT dht(2, DHT11);
+int tempHum[2];
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.setDataRate(RF24_250KBPS);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.openWritingPipe(address[0]);
+  radio.setChannel(0x60);
+  dht.begin(); 
+  radio.powerUp();
+  radio.stopListening();
+}
+
+void loop() {
+  int h = dht.readHumidity();
+  int t = dht.readTemperature();
+  tempHum[0] = t;
+  tempHum[1] = h;
+  radio.write(&tempHum, sizeof(tempHum));
+  delay(1000);
+  // Serial.print(1);
+  // Serial.print(address[0]);
+  // delay(2000);
+  // Serial.print(t);
+  // Serial.print("°C");
+  // Serial.print(" ");
+  // Serial.print("ВЛАЖНОСТЬ ");
+  // Serial.print(h);
+  // Serial.println("%");
+}
+/*   Данный скетч делает следующее: передатчик (TX) отправляет массив
+     данных, который генерируется согласно показаниям с кнопки и с
+     двух потенциомтеров. Приёмник (RX) получает массив, и записывает
+     данные на реле, сервомашинку и генерирует ШИМ сигнал на транзистор.
+    by AlexGyver 2016
+*/
+
+// #include <SPI.h>          // библиотека для работы с шиной SPI
+// #include "nRF24L01.h"     // библиотека радиомодуля
+// #include "RF24.h"         // ещё библиотека радиомодуля
+
+// RF24 radio(9, 10); // "создать" модуль на пинах 9 и 10 Для Уно
+// //RF24 radio(9,53); // для Меги
+
+// byte address[][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"}; //возможные номера труб
+
+// byte counter;
+
+// void setup() {
+//   Serial.begin(9600);         // открываем порт для связи с ПК
+
+//   radio.begin();              // активировать модуль
+//   radio.setAutoAck(1);        // режим подтверждения приёма, 1 вкл 0 выкл
+//   radio.setRetries(0, 15);    // (время между попыткой достучаться, число попыток)
+//   radio.enableAckPayload();   // разрешить отсылку данных в ответ на входящий сигнал
+//   radio.setPayloadSize(32);   // размер пакета, в байтах
+
+//   radio.openWritingPipe(address[0]);  // мы - труба 0, открываем канал для передачи данных
+//   radio.setChannel(0x60);             // выбираем канал (в котором нет шумов!)
+
+//   radio.setPALevel (RF24_PA_MAX);   // уровень мощности передатчика. На выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
+//   radio.setDataRate (RF24_250KBPS); // скорость обмена. На выбор RF24_2MBPS, RF24_1MBPS, RF24_250KBPS
+//   //должна быть одинакова на приёмнике и передатчике!
+//   //при самой низкой скорости имеем самую высокую чувствительность и дальность!!
+
+//   radio.powerUp();        // начать работу
+//   radio.stopListening();  // не слушаем радиоэфир, мы передатчик
+// }
+
+// void loop() {
+//   Serial.print("Sent: ");
+//   Serial.println(counter);
+//   radio.write(&counter, sizeof(counter));
+//   counter++;
+//   delay(10);
+// }
+
+
